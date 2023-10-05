9fbf88d5c58a7848166e68698b2901cce095f7a6
mitsusaburo <bschindlerm@gmail.com>
2023-10-05 11:17:07 -0300

    User input from button

--- scripts.js
+++ scripts.js
@@ -1,18 +1,30 @@
 let container = document.querySelector(".container");
 
-// let gridSize = prompt("What's the size of the square?");
-let gridSize = 16;
 
-for (let i = 0; i < gridSize; i++) {
+
+
+let submitSquares = document.querySelector("#example");
+
+// Creates the grid for canvas from input, fires after click event
+submitSquares.addEventListener("click", () => {
+    container.innerHTML = "";
+    let text = document.querySelector("#squaresNumber");
+    gridSize = text.value;
+    // console.log(gridSize);
     for (let i = 0; i < gridSize; i++) {
-        let square = document.createElement("div");
-        square.className = "square";
-        container.appendChild(square);
-    }   
-}
+        for (let i = 0; i < gridSize; i++) {
+            let square = document.createElement("div");
+            square.className = "square";
+            container.appendChild(square);
+        }   
+    }
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((sq) => {
+        sq.style.backgroundColor = "black";
+    }); 
+})
 
-let squares = document.querySelectorAll(".square");
 
-squares.forEach((sq) => {
-    sq.style.backgroundColor = "white";
-});
+
+
+
